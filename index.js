45578d20162be55cb59806137b0bf692cec4e3fd
BulliedDog <albertorovai1@gmail.com>
2024-10-09 19:22:01 +0200

    throwing in some js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('hello world!')
+var text=document.getElementById("text")
+function change_text(){
+    //if(text.innerHTML="Awesome!")
+        text.innerHTML="Sghebs gay"
+    //else
+    //    text.innerHTML="Awesome!"
+}
